d75926030d773150c7d053b63b71ac4c1c1be778
André Ferreira <contato@andreferreira.me>
2018-03-26 15:42:53 -0300

    python submit isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,11 +1,11 @@
 def is_isogram(string):
-    print(list(range(len(string))))
-    # for i in range(len(string)):
-    #     for j in range(len(string)):
-    #         list(string).count()
-    #         print(list(string)[i], i)
-    #         # print(list(string)[j], j)
-    # return True
+    string = string.lower()
 
-print(is_isogram(""))
-print(is_isogram("isogram"))
+    for i in range(len(string)):
+        if not string[i].isalpha():
+            break
+
+        if (list(string).count(string[i]) > 1):
+            return False
+            
+    return True
