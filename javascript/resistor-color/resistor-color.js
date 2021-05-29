d6301702348e26491e23ca280d97dc9b1bf39f6c
André Ferreira <contato@andreferreira.me>
2021-05-29 02:39:21 -0300

    Resistor Color solution

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const colorCode = (color) => {
+  switch (color) {
+    case "black":
+      return 0;
+    case "white":
+      return 9;
+    case "orange":
+      return 3;
+  }
 };
 
-export const COLORS = undefined;
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
