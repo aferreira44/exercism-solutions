12198192f12b3ca47cb252447261757afcc5587d
André Ferreira <contato@andreferreira.me>
2021-05-30 03:06:08 -0300

    Run tests

--- typescript/word-count/word-count.test.ts
+++ typescript/word-count/word-count.test.ts
@@ -8,12 +8,12 @@ describe('words()', () => {
     expect(words.count('word')).toEqual(expectedCounts)
   })
 
-  xit('counts one of each', () => {
+  it('counts one of each', () => {
     const expectedCounts = new Map(Object.entries({ one: 1, of: 1, each: 1 }))
     expect(words.count('one of each')).toEqual(expectedCounts)
   })
 
-  xit('counts multiple occurrences', () => {
+  it('counts multiple occurrences', () => {
     const expectedCounts = new Map(
       Object.entries({ one: 1, fish: 4, two: 1, red: 1, blue: 1 })
     )
@@ -22,7 +22,7 @@ describe('words()', () => {
     )
   })
 
-  xit('includes punctuation', () => {
+  it('includes punctuation', () => {
     const expectedCounts = new Map(
       Object.entries({
         car: 1,
@@ -38,46 +38,46 @@ describe('words()', () => {
     )
   })
 
-  xit('includes numbers', () => {
+  it('includes numbers', () => {
     const expectedCounts = new Map(Object.entries({ testing: 2, 1: 1, 2: 1 }))
     expect(words.count('1 2 testing testing')).toEqual(expectedCounts)
   })
 
-  xit('normalizes to lower case', () => {
+  it('normalizes to lower case', () => {
     const expectedCounts = new Map(Object.entries({ go: 3 }))
     expect(words.count('go Go GO')).toEqual(expectedCounts)
   })
 
-  xit('counts properly international characters', () => {
+  it('counts properly international characters', () => {
     const expectedCounts = new Map(
       Object.entries({ '¡hola!': 1, '¿qué': 1, 'tal?': 1, 'привет!': 1 })
     )
     expect(words.count('¡Hola! ¿Qué tal? Привет!')).toEqual(expectedCounts)
   })
 
-  xit('counts multiline', () => {
+  it('counts multiline', () => {
     const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
     expect(words.count('hello\nworld')).toEqual(expectedCounts)
   })
 
-  xit('counts tabs', () => {
+  it('counts tabs', () => {
     const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
     expect(words.count('hello\tworld')).toEqual(expectedCounts)
   })
 
-  xit('counts multiple spaces as one', () => {
+  it('counts multiple spaces as one', () => {
     const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
     expect(words.count('hello  world')).toEqual(expectedCounts)
   })
 
-  xit('does not count leading or trailing whitespace', () => {
+  it('does not count leading or trailing whitespace', () => {
     const expectedCounts = new Map(
       Object.entries({ introductory: 1, course: 1 })
     )
     expect(words.count('\t\tIntroductory Course      ')).toEqual(expectedCounts)
   })
 
-  xit('handles properties that exist on Object’s prototype', () => {
+  it('handles properties that exist on Object’s prototype', () => {
     const expectedCounts = new Map(
       Object.entries({
         reserved: 1,
