0a5bf7c7e9ed812e5f5b2bafdf1b3c682493f958
André Ferreira <contato@andreferreira.me>
2021-05-30 23:22:47 -0300

    Gigasecond solution

--- typescript/gigasecond/gigasecond.ts
+++ typescript/gigasecond/gigasecond.ts
@@ -1,7 +1,15 @@
 class Gigasecond {
-  date(/* Parameters go here */) {
-    // Your code here
+  _date: Date;
+
+  constructor(date: Date) {
+    this._date = date;
+  }
+
+  date(): Date {
+    const gigasecondDate = new Date(this._date);
+    gigasecondDate.setSeconds(this._date.getSeconds() + Math.pow(10, 9));
+    return gigasecondDate;
   }
 }
 
-export default Gigasecond
+export default Gigasecond;
